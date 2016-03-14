d0e6b75aad188058b460229f8ee90f7cf05c690b
Teon <teon.prudent@gmail.com>
2016-03-14 16:43:36 -0400

    adds dist tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
-//var react       = require('gulp-react');
 var concat      = require('gulp-concat');
 var gutil		= require('gulp-util');
 var source      = require('vinyl-source-stream');
 var browserify  = require('browserify');
 var watchify    = require('watchify');
 var reactify    = require('reactify');
+var del         = require('del');
+var usemin      = require('gulp-usemin');
+var sourcemaps  = require('gulp-sourcemaps');
+var minifyCss   = require('gulp-minify-css');
+var rev         = require('gulp-rev');
+var minify      = require('gulp-minify');
+var uglify      = require('gulp-uglify');
+var htmlmin     = require('gulp-htmlmin');
+var streamify   = require('gulp-streamify');
+
+// Paths 
+var config = require('./gulp-config.json');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'ify'], function() {
@@ -16,26 +27,22 @@ gulp.task('serve', ['sass', 'ify'], function() {
         server: "./app"
     });
 
-    gulp.watch("app/styles/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/scripts/*.js").on('change', browserSync.reload);
+    gulp.watch(config.paths.sassFiles, ['sass']);
+    gulp.watch(config.paths.indexFiles).on('change', browserSync.reload);
+    gulp.watch(config.paths.jsFiles).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/styles/*.scss")
+    return gulp.src(config.paths.sassFiles)
+    	.pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(gulp.dest("app/styles"))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(config.paths.stylesOutput))
         .pipe(browserSync.stream());
 });
 
-gulp.task('react', function() {
-	return gulp.src(['app/scripts/*.jsx'])
-	.pipe(react())
-	.pipe(concat('app.js'))
-	.pipe(gulp.dest('app/scripts'));
-});
-
+// watchify, reactify, browserify magic
 gulp.task('ify', function() {
 	var bundler = watchify(browserify({
 		entries: ['./app/scripts/app.jsx'], //main entry point
@@ -51,11 +58,48 @@ gulp.task('ify', function() {
 		return bundler
 			.bundle()
 			.on('error', gutil.log.bind(gutil, 'Browserify Error'))
-			.pipe(source('app/scripts/app.js'))
+			.pipe(source(config.paths.mainAppJs))
 			.pipe(gulp.dest('./'));
-	};
-	build()
-	bundler.on('update', build)
+	}
+	build();
+	bundler.on('update', build);
+});
+
+
+// 'clean' clean dist directory before a new build
+gulp.task('clean', require('del').bind(null, config.paths.distFolder));
+
+// 'minify Main App,js' move necessary files to dist
+gulp.task('minAppJs', function() {
+	gulp.src(config.paths.mainAppJs)
+	.pipe(streamify(uglify()))
+	.pipe(gulp.dest(config.paths.distFolder + config.paths.jsDir));
+});
+
+// 'usemin' looks at 'html' files for the doc blocks to concatenate css & js, uglify js, minifies html, adds hashes to js & css to bypass caches  
+gulp.task('usemin', function() {
+	gulp.src(config.paths.indexFiles)
+		.pipe(usemin({
+			css: [
+				sourcemaps.init(),
+				minifyCss(),
+				'concat',
+				rev(),
+				sourcemaps.write(config.paths.distSourcemaps)
+			],
+			js: [
+				sourcemaps.init(),
+				minify(),
+				rev(),
+				sourcemaps.write(config.paths.distSourcemaps)
+			],
+			bowerJs: [
+
+			]
+		}))
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest(config.paths.distFolder));
 });
 
 gulp.task('default', ['serve']);
+gulp.task('deploy', ['clean', 'minAppJs', 'usemin']);

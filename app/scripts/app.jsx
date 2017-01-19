feaebfc34e04489bfa86d8f0361c049c41a844e0
Teon <teon.prudent@gmail.com>
2017-01-19 14:32:33 -0500

    es6 conversion

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ImageBox from './imageBox';
+
+const data = [ 
+			{id: 1, title: 'The Penny Drop café', image: 'http://68.media.tumblr.com/0e5dafee0184cbbdb9347c75ce9f62ff/tumblr_oi1wfaCWsx1qk9hrqo1_1280.jpg'}
+			];
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<ImageBox />
+			</div>
+		)
+	}
+}
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('app')
 );
